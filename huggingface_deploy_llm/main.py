e55d945b9a3780b57f82c995bcde1a929661b5a3
Ederson Melo <edersonmelo@outlook.com>
2024-06-19 10:38:07 -0300

    Add files via upload

--- huggingface_deploy_llm/main.py
+++ huggingface_deploy_llm/main.py
@@ -8,13 +8,14 @@ app = FastAPI()
 llm = None
 
 def start_llm():
+    global llm  # Adicione esta linha para modificar a variável global
     llm = Llama(model_path="./tinyllama-1.1b-chat.gguf")
     
- 
 @app.post("/health")
+def health_check():
     return {"status": "ok"}
 
-@app.post("/llm")
+@app.post("/deployllm")
 async def stream(item: dict):
     
     if llm is None:
@@ -23,11 +24,10 @@ async def stream(item: dict):
     if 'prompt' not in item.keys():
         raise ValueError("prompt é obrigatório")
 
-    prompt = item['prompt']
+    prompt = "<|system|>You are a helpfull assistant</s><|user|>"+item['prompt']+"</s><|assistant|>"
     temperatura = item['temperatura'] if 'temperatura' in item.keys() else 0.2
     max_tokens = item['max_tokens'] if 'max_tokens' in item.keys() else 512
     
     return llm(prompt, max_tokens=max_tokens, temperature=temperatura)
-    
     
 threading.Thread(target=start_llm).start()
